80755ec06f3b1f40dea93ea1972c1a79314e58db
Antoine Faravelon <antoine.faravelon@greenwaves-technologies.com>
2019-07-30 11:38:40 +0200

    update subs with compatibility fixes

--- platform/mcu/gap8/aos.mk
+++ platform/mcu/gap8/aos.mk
@@ -27,7 +27,7 @@ GLOBAL_LDFLAGS += -Wl,--gc-sections   \
                   --specs=nosys.specs \
                   -usystem_vectors
 
-$(NAME)_CFLAGS += -Wall -Werror -Wno-unused-variable -Wno-unused-parameter -Wno-implicit-function-declaration
+$(NAME)_CFLAGS += -Wall -Wno-unused-variable -Wno-unused-parameter -Wno-implicit-function-declaration
 $(NAME)_CFLAGS += -Wno-type-limits -Wno-sign-compare -Wno-pointer-sign -Wno-uninitialized
 $(NAME)_CFLAGS += -Wno-return-type -Wno-unused-function -Wno-unused-but-set-variable
 $(NAME)_CFLAGS += -Wno-unused-value -Wno-strict-aliasing
