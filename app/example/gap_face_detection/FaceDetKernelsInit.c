f01213fbd99651e180721c6b007535df104eb629
Yao ZHANG <yao.zhang@greenwaves-technologies.com>
2019-08-07 12:54:36 +0200

    removed generated files

--- app/example/gap_face_detection/FaceDetKernelsInit.c
+++
@@ -1,9 +0,0 @@
-#include "Gap8.h"
-#include "AutoTilerLibTypes.h"
-L1_CL_MEM char *FaceDet_L1_Memory;
-L2_MEM char *FaceDet_L2_Memory;
-Kernel_Arg_Exec_T FaceDet_KernelArgs[] = {
-};
-
-Kernel_Exec_T FaceDet_KernelDescr[] = {
-};
--- app/example/gap_face_detection/FaceDetKernelsInit.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FACEDETKERNELSINI_H__
-#define __FACEDETKERNELSINI_H__
-
-#include "AutoTilerLibTypes.h"
-extern Kernel_Exec_T FaceDet_KernelDescr[];
-
-
-
-#endif
