3a06a1cfd8f1e1a00ba63765ed432a311fe4ac32
Antoine Faravelon <antoine.faravelon@greenwaves-technologies.com>
2019-10-15 15:27:35 +0200

    remove pmsis examples from aos examples

--- app/example/gap_ble/aos.mk
+++
@@ -1,8 +0,0 @@
-NAME := gap_ble
-
-$(NAME)_MBINS_TYPE := app
-$(NAME)_VERSION := 1.0.0
-$(NAME)_SUMMARY := this a sample of ble over uart for gap8
-$(NAME)_SOURCES := test_ble.c
-
-GLOBAL_DEFINES += AOS_NO_WIFI
--- app/example/gap_camera/aos.mk
+++
@@ -1,9 +0,0 @@
-NAME := gap_camera
-
-$(NAME)_MBINS_TYPE := app
-$(NAME)_VERSION := 1.0.0
-$(NAME)_SUMMARY := this a sample of camera for gap8
-$(NAME)_SOURCES := test_cam.c
-
-GLOBAL_DEFINES += AOS_NO_WIFI
-GLOBAL_CFLAGS += -D__PMSIS__
