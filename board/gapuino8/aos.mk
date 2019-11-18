4fe0b22ace64dbc1819e331c16e1e31a7e78d870
Antoine Faravelon <antoine.faravelon@greenwaves-technologies.com>
2019-11-18 11:13:11 +0100

    GAPUINO8:     Board build cleanups

--- board/gapuino8/aos.mk
+++ board/gapuino8/aos.mk
@@ -19,16 +19,13 @@ GLOBAL_LDFLAGS +=
 GLOBAL_INCLUDES += .
 
 GLOBAL_INCLUDES += ../../platform/mcu/gap8/
-#GLOBAL_INCLUDES += ../../platform/mcu/gap8/include/
 GLOBAL_INCLUDES += ../../platform/mcu/gap8/drivers/
 
 $(NAME)_SOURCES     := ./board.c
-#$(NAME)_SOURCES     += ./clock_config.c
-#$(NAME)_SOURCES     += ./pin_mux.c
 
+$(NAME)_COMPONENTS += yloop vfs
 # include pmsis stuff
 BOARD_NAME=gapuino8
-#include ./platform/mcu/gap8/pmsis.mk
 # configure for soc and board
 GLOBAL_CFLAGS += -D__GAP8__ -DGAPUINO8 -fdata-sections -ffunction-sections -Os -g -DCONFIG_GAPUINO
 GLOBAL_LDFLAGS += -Wl,--gc-sections -Os -g -DCONFIG_GAPUINO
@@ -38,4 +35,3 @@ GLOBAL_CFLAGS += -DHIMAX -DPRINTF_USE_UART
 
 TEST_COMPONENTS += testcase.kernel.rhino testcase.kernel.basic aos_test
 GLOBAL_CFLAGS += -DTEST_CONFIG_KV_ENABLED=0
-GLOBAL_CFLAGS += -DTEST_CONFIG_YLOOP_ENABLED=0
