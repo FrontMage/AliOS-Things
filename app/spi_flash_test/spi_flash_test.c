9fbfae940bfe3f66fdad88aa5911a9fb6743359d
Antoine Faravelon <antoine.faravelon@greenwaves-technologies.com>
2019-11-26 10:34:49 +0100

    WIP: spi flash test, work in single mode

---
+++ app/spi_flash_test/spi_flash_test.c
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#include <stdio.h>
+
+#include "pmsis.h"
+#include "gap_semihost.h"
+#include <aos/kernel.h>
+
+PI_L2 char write_string[] = {'h','e','l','l','o',' ','f','r','i','e','n','d','s','\n','\0'};
+
+PI_L2 volatile uint8_t data[256+5];
+PI_L2 volatile uint8_t data_r[256] = {0};
+
+PI_L2 pi_device_t qspi_dev0 = {0};
+
+PI_L2 uint32_t g_zero = 0;
+
+#define QSPIF_DUMMY_CYCLES(x)   ((uint8_t)((x&0xF)<<3))
+#define QSPIF_BURST_LEN(x)      ((uint8_t)((x&0x3)<<0))
+#define QSPIF_BURST_ENA(x)      ((uint8_t)((x&0x1)<<1))
+
+#define QSPIF_WR_READ_REG_CMD   ((uint8_t)0x63)
+#define QSPIF_WR_EN_CMD         ((uint8_t)0x06)
+#define QSPIF_QPI_EN_CMD        ((uint8_t)0x35)
+#define QSPIF_QPI_FAST_READ     ((uint8_t)0x0b) 
+
+#define DUMMY_CYCLES 3
+
+//uint8_t g_set_qspif_dummy[] = {QSPIF_WR_EN_CMD, QSPIF_WR_READ_REG_CMD,
+//    QSPIF_DUMMY_CYCLES(DUMMY_CYCLES) | QSPIF_BURST_LEN(3) | QSPIF_BURST_ENA(1)};
+// set 2 dummy cycles @ 50MHz
+uint8_t g_set_qspif_dummy[] = {QSPIF_WR_READ_REG_CMD,
+    QSPIF_DUMMY_CYCLES(DUMMY_CYCLES)};
+uint8_t g_enter_qpi_mode[] = {0,0,0,0};
+
+uint8_t g_erase_block_0[] = {0xD7, 0, 0x10, 0, 0, 0, 0,0};
+uint8_t g_buf[8] = {0};
+
+int application_start(int argc, char *argv[])
+{
+    int count = 0;
+    int errors = 0;
+    printf("nano entry here!\n");
+
+    aos_msleep(1000);
+    // Add wr enable, then send cmd, addr, and finally, data from index 5
+    //data[0] = QSPIF_WR_EN_CMD;
+    data[0] = 0x02;
+    data[1] = 0x0;
+    data[2] = 0x10;
+    data[3] = 0x0;
+
+    for(int i = 0; i<256; i++)
+    {
+        data[i+4] = i+1;
+    }
+
+    struct pi_spi_conf spi_conf;
+    memset(&spi_conf, 0, sizeof(struct pi_spi_conf));
+
+    pi_spi_conf_init(&spi_conf);
+    spi_conf.max_baudrate = system_core_clock_get(); // 50MHz for now
+    spi_conf.wordsize = PI_SPI_WORDSIZE_8;
+    spi_conf.polarity = 0;
+    spi_conf.phase = spi_conf.polarity;
+    spi_conf.cs = 0;
+    spi_conf.itf = 0;
+    spi_conf.big_endian = 1;
+
+    pi_open_from_conf(&qspi_dev0, &spi_conf);
+    int ret = pi_spi_open(&qspi_dev0);
+
+    printf("ret=%x\n",ret);
+
+    // init flash --> setup dummy cycles first
+    pi_spi_flags_e flag =  PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO;
+#if 0
+    g_buf[0] = 0x66;
+    pi_spi_transfer(&qspi_dev0, (void*)&g_buf[0], (void*)&g_buf[4],8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+    aos_msleep(100);
+    g_buf[0] = 0x99;
+    pi_spi_transfer(&qspi_dev0, (void*)&g_buf[0], (void*)&g_buf[4],8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+    aos_msleep(100);
+    printf("------------Reset done-------------\n");
+#endif
+    
+    //g_buf[0] = QSPIF_WR_EN_CMD;
+    //pi_spi_transfer(&qspi_dev0, (void*)g_buf, (void*)&g_buf[4],8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+
+    g_buf[0] = 0x9F;
+    pi_spi_transfer(&qspi_dev0, (void*)&g_buf[0], (void*)&g_buf[4], 8, PI_SPI_LINES_SINGLE | PI_SPI_CS_KEEP);
+    pi_spi_transfer(&qspi_dev0, (void*)&g_zero, (void*)&g_buf[0], 8*3, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+    printf("jdec id reg= %x\n",g_buf[0]);
+    printf("jdec id reg= %x\n",g_buf[1]);
+    printf("jdec id reg= %x\n",g_buf[2]);
+
+    //pi_spi_send(&qspi_dev0, (void*)g_set_qspif_dummy, 2*8, flag);
+    //g_buf[0] = 0x61;
+    //pi_spi_send(&qspi_dev0, (void*)g_buf, 1*8, PI_SPI_LINES_SINGLE | PI_SPI_CS_KEEP);
+    //pi_spi_receive(&qspi_dev0, (void*)g_buf, 1*8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+    //printf("read param reg= %x\n",g_buf[0]),
+    // then QPI mode enter
+    //printf("g_enter_qpi_mode: %x\n",g_enter_qpi_mode);
+    //pi_spi_send(&qspi_dev0, (void*)g_enter_qpi_mode, 1*8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+    // flash is now in QPI mode, with 2 dummy read cycles
+    // -------------------------------------------------
+    
+    flag =  PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO;
+    // Erase block 0
+    
+    g_buf[0] = QSPIF_WR_EN_CMD;
+    pi_spi_transfer(&qspi_dev0, (void*)g_buf, (void*)&g_buf[4],8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+
+    pi_spi_send(&qspi_dev0, (void*)g_erase_block_0, 4*8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+    uint8_t wip = 0;
+    do
+    {
+        g_buf[0] = 0x05;
+        pi_spi_send(&qspi_dev0, (void*)g_buf, 1*8, PI_SPI_LINES_SINGLE | PI_SPI_CS_KEEP);
+        pi_spi_receive(&qspi_dev0, (void*)g_buf, 1*8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+        printf("g_buf[0]=%i\n",g_buf[0]);
+        wip = g_buf[0] & 0x1;
+    }while(wip);
+    // Write
+    g_buf[0] = QSPIF_WR_EN_CMD;
+    pi_spi_transfer(&qspi_dev0, (void*)g_buf, (void*)&g_buf[4],8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+    pi_spi_send(&qspi_dev0, (void*)data, 260*8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO); 
+    wip = 0;
+    do
+    {
+        g_buf[0] = 0x05; // read status reg
+        pi_spi_send(&qspi_dev0, (void*)g_buf, 1*8, PI_SPI_LINES_SINGLE | PI_SPI_CS_KEEP);
+        pi_spi_receive(&qspi_dev0, (void*)g_buf, 1*8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+        printf("g_buf[0]=%i\n",g_buf[0]);
+        wip = g_buf[0] & 0x1;
+    }while(wip);
+
+    // now, read back the data
+    g_buf[0] = 0x3;
+    g_buf[1] = 0;
+    g_buf[2] = 0x10;
+    g_buf[3] = 0;
+    //*(uint32_t*)(&g_buf[4]) = SPI_CMD_DUMMY(DUMMY_CYCLES);
+
+    printf("data_r: %x\n",data_r);
+    pi_spi_send(&qspi_dev0, (void*)g_buf, 8*4, PI_SPI_LINES_SINGLE | PI_SPI_CS_KEEP);
+    pi_spi_receive(&qspi_dev0, (void*)data_r, 256*8, PI_SPI_LINES_SINGLE | PI_SPI_CS_AUTO);
+
+    for(int i = 0; i<256; i++)
+    {
+        if(data_r[i] != data[i+4])
+        {
+            printf("data_r[%i]=%x, data[%i+5]=%x\n",i,data_r[i],i,data[i+5]);
+            errors ++;
+        }
+    }
+
+    printf("there was %d errors\n",errors);
+    return errors;
+}
